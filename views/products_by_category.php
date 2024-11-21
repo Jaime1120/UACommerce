ad2339e77f2f26ca80dd5706c5be7f6c9457a71d
cleomari <al062090@uacam.mx>
2024-11-21 01:50:27 -0600

    Diseño para mostrar los productos de una categoria en especifico

--- views/products_by_category.php
+++ views/products_by_category.php
@@ -18,6 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categorías - UACommerce</title>
     <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="ProductByCategory.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
 </head>
